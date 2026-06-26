70b9e7b7b5d67985296deaefcc819feaa764b7a3
HasanRazaRawjani <rawjanihasan@gmail.com>
2026-06-26 19:20:41 -0400

    committt

---
+++ Assets/Scripts/Planets/PlanetOrbit.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlanetOrbit : MonoBehaviour
+{
+    [Header("Orbit Settings")]
+    public Transform sunTarget;          
+    public float orbitalPeriodInDays;    
+
+    
+    public static float timeScaleMultiplier = 10f;
+
+    void Start()
+    {
+        if (sunTarget == null)
+        {
+            
+            GameObject sun = GameObject.Find("Sun Sphere");
+            if (sun != null) sunTarget = sun.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (sunTarget == null) return;
+
+        
+        float speed = (360f / orbitalPeriodInDays) * timeScaleMultiplier;
+
+        
+        transform.RotateAround(sunTarget.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
